858a97ac42c1f1664e084f373c71d72c8c00fc45
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-19 13:16:51 +0200

    current

--- code/testing.py
+++ code/testing.py
@@ -1,38 +1,8 @@
-import random
-
-from neuron import Neuron
-import os
-
-seed = None
-size = 40
-
-
-def setup_seed():
-    global seed, size
-    seed = os.urandom(size)
-
-
-def get_seed():
-    global seed
-    return seed
-
-
-def insert_seed():
-    random.seed(a=get_seed())
+from sigmoid import sigmoid
 
 
 def main():
-    setup_seed()
-    insert_seed()
-
-    print('testing')
-    print(random.uniform(1.0, 200.0))
-
-    n = Neuron()
-    n.set_input(2)
-
-    n.calculate()
-    print(n.get_output())
+    print(sigmoid(5.0))
     pass
 
 
