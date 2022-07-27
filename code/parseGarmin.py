79efe21ee89a006d2eca03e36a8123073ba6b012
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-27 15:15:03 +0200

    going forward

--- code/parseGarmin.py
+++ code/parseGarmin.py
@@ -1,4 +1,6 @@
-import os
+import os, \
+       datetime, \
+       pytz
 
 
 def retrieve_fit_files(fullPathVar):
@@ -9,3 +11,12 @@ def retrieve_fit_files(fullPathVar):
         array.append(fullPath)
 
     return array
+
+
+def get_timestamp(last_timestamp, timestamp_16):
+    timestamp = int(datetime.datetime.timestamp(last_timestamp)) - 631065600
+    mesg = timestamp
+    mesg += (timestamp_16 - (mesg & 0xFFFF)) & 0xFFFF
+
+    result = datetime.datetime.fromtimestamp((mesg + 631065600), pytz.timezone('Europe/Copenhagen'))
+    return result
